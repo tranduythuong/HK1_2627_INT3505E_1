8458e6877f3659fa563cac324371f56a7781df95
tranduythuong <24021638@vnu.edu.vn>
2026-09-19 21:24:00 +0700

    19/9/2026

---
+++ books_dump.sql
@@ -0,0 +1,15 @@
+BEGIN TRANSACTION;
+CREATE TABLE books (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        author TEXT NOT NULL,
+        title TEXT NOT NULL
+    );
+INSERT INTO "books" VALUES(1,'Nguyen Van A','Python Co Ban');
+INSERT INTO "books" VALUES(2,'Nguyen Van B','Flask REST API');
+INSERT INTO "books" VALUES(3,'Robert','RESTful Web Services');
+INSERT INTO "books" VALUES(4,'Nguyen Van A','Python Co Ban');
+INSERT INTO "books" VALUES(5,'Nguyen Van B','Flask REST API');
+INSERT INTO "books" VALUES(6,'Robert','RESTful Web Services');
+DELETE FROM "sqlite_sequence";
+INSERT INTO "sqlite_sequence" VALUES('books',6);
+COMMIT;
